af97f1971c96d765b1e5ada0569797beb8815df7
Louis Goessling <louis@goessling.com>
2022-03-14 15:57:08 -0500

    Clean up start/stop logic

--- src/server.cpp
+++ src/server.cpp
@@ -112,11 +112,11 @@ void ParseScpiLine(const string& line, string& subject, string& cmd, bool& query
 	}
 }
 
-std::vector<struct sr_channel*> g_channels;
+vector<struct sr_channel*> g_channels;
 int g_numdivs;
 
 bool g_runAcq;
-bool g_triggerOneShot;
+bool g_triggerOneShot = false;
 
 void run_server(struct sr_dev_inst *device, int scpi_port) {
 	for (GSList *l = device->channels; l; l = l->next) {
@@ -127,13 +127,19 @@ void run_server(struct sr_dev_inst *device, int scpi_port) {
     g_numdivs = DS_CONF_DSO_VDIVS;
 	// TODO: SR_CONF_NUM_VDIV instead of DS_CONF_DSO_VDIVS on regular sigrok
 
-	std::vector<uint64_t> vdiv_options = get_dev_config_options<uint64_t>(device, SR_CONF_PROBE_VDIV);
-	std::vector<uint64_t> rate_options = get_dev_config_options<uint64_t>(device, SR_CONF_SAMPLERATE);
+	// set_dev_config<bool>(device, SR_CONF_STREAM, true);
+	// TODO: What does this do? Do we care?
+
+	vector<uint64_t> vdiv_options = get_dev_config_options<uint64_t>(device, SR_CONF_PROBE_VDIV);
+	vector<uint64_t> rate_options = get_dev_config_options<uint64_t>(device, SR_CONF_SAMPLERATE);
 
 	string samplerates_string;
 	for (auto i : rate_options) {
 		samplerates_string += to_string(i) + ",";
 	}
+
+
+	set_dev_config<uint64_t>(device, SR_CONF_LIMIT_SAMPLES, 10000000);
 
 	uint64_t curr_bufsz = get_dev_config<uint64_t>(device, SR_CONF_LIMIT_SAMPLES).value();
 
@@ -224,71 +230,135 @@ void run_server(struct sr_dev_inst *device, int scpi_port) {
 			}
 			else
 			{
-				if ((cmd == "ON" || cmd == "OFF") && subj_ch && args.size() == 0) {
-					set_probe_config<bool>(device, subj_ch, SR_CONF_PROBE_EN, cmd == "ON");
-					LogDebug("Updated enabled for ch%s, now %s\n", subject.c_str(), cmd.c_str());
-				} else if (cmd == "COUP" && subj_ch && args.size() == 1) {
-					string coup_str = args[0];
-					uint8_t sr_coupling = -1;
+				// Channel commands
+				if (subj_ch) {
+					if ((cmd == "ON" || cmd == "OFF") && args.size() == 0) {
+						bool state = cmd == "ON";
+						if (!state && subj_ch == g_channels[0]) {
+							LogWarning("Ignoring request to disable ch0\n");
+						} else {
+							set_probe_config<bool>(device, subj_ch, SR_CONF_PROBE_EN, state);
+							LogDebug("Updated enabled for ch%s, now %s\n", subject.c_str(), cmd.c_str());
 
-					if (coup_str == "AC1M") {
-						sr_coupling = SR_AC_COUPLING;
-					} else if (coup_str == "DC1M") {
-						sr_coupling = SR_DC_COUPLING;
-					} else {
-						LogWarning("Unknown coupling: %s\n", coup_str.c_str());
-						continue;
-					}
-
-					set_probe_config<uint8_t>(device, subj_ch, SR_CONF_PROBE_COUPLING, sr_coupling);
-					LogDebug("Updated coupling for ch%s, now %s\n", subject.c_str(), coup_str.c_str());
-				} else if (cmd == "RANGE" && subj_ch && args.size() == 1) {
-					float range_V = stod(args[0]);
-					float range_mV_per_div = (range_V / g_numdivs) * 1000;
-
-					uint64_t selected = vdiv_options[vdiv_options.size()];
-
-					for (auto i : vdiv_options) {
-						if ((i > selected) && !(i > range_mV_per_div)) {
-							selected = i;
+							if (g_runAcq) {
+								sr_session_stop(); // Restart acquisition
+								g_runAcq = true;
+							}
 						}
-					}
+					} else if (cmd == "COUP" && args.size() == 1) {
+						string coup_str = args[0];
+						uint8_t sr_coupling = -1;
 
-					set_probe_config<uint64_t>(device, subj_ch, SR_CONF_PROBE_VDIV, selected);
-					LogDebug("Updated RANGE; Wanted %f, result: %lu\n", range_mV_per_div, selected);
-				} else if (cmd == "OFFS" && subj_ch && args.size() == 1) {
-					LogDebug("Ignored OFFS\n");
-				} else if (cmd == "RATE" && subj_dev && args.size() == 1) {
-					uint64_t rate = stoull(args[0]);
-					set_dev_config<uint64_t>(device, SR_CONF_SAMPLERATE, rate);
-					LogDebug("Updated RATE; now %lu\n", rate);
-				} else if (cmd == "DEPTH" && subj_dev && args.size() == 1) {
-					uint64_t depth = stoull(args[0]);
-					set_dev_config<uint64_t>(device, SR_CONF_LIMIT_SAMPLES, depth);
-					LogDebug("Updated DEPTH; now %lu\n", depth);
-				} else if ((cmd == "START" || cmd == "SINGLE") && subj_dev && args.size() == 0) {
-					LogDebug("Starting %s...\n", cmd.c_str());
-					g_triggerOneShot = cmd == "SINGLE";
-					g_runAcq = true;
-				} else if (cmd == "STOP" && subj_dev && args.size() == 0) {
-					LogDebug("Stopping...\n");
-					g_runAcq = 0;
-					sr_session_stop();
-				} else {
-					LogWarning("Unknown or malformed command: %s\n", line.c_str());
+						if (coup_str == "AC1M") {
+							sr_coupling = SR_AC_COUPLING;
+						} else if (coup_str == "DC1M") {
+							sr_coupling = SR_DC_COUPLING;
+						} else {
+							LogWarning("Unknown coupling: %s\n", coup_str.c_str());
+							continue;
+						}
+
+						set_probe_config<uint8_t>(device, subj_ch, SR_CONF_PROBE_COUPLING, sr_coupling);
+						LogDebug("Updated coupling for ch%s, now %s\n", subject.c_str(), coup_str.c_str());
+					} else if (cmd == "RANGE" && args.size() == 1) {
+						float range_V = stod(args[0]);
+						float range_mV_per_div = (range_V / g_numdivs) * 1000;
+
+						uint64_t selected = vdiv_options[vdiv_options.size()];
+
+						for (auto i : vdiv_options) {
+							if ((i > selected) && !(i > range_mV_per_div)) {
+								selected = i;
+							}
+						}
+
+						set_probe_config<uint64_t>(device, subj_ch, SR_CONF_PROBE_VDIV, selected);
+						LogDebug("Updated RANGE; Wanted %f, result: %lu\n", range_mV_per_div, selected);
+					} else if (cmd == "OFFS" && args.size() == 1) {
+						LogDebug("Ignored OFFS\n");
+					} else {
+						LogWarning("Unknown command: %s\n", line.c_str());
+					}
+				}
+
+				// Trigger commands
+				if (subj_trig) {
+					if (cmd == "DELAY") {
+						
+					} else if (cmd == "SOU") {
+						
+					} else if (cmd == "LEV" && args.size()==1) {
+						double level = stod(args[0]);
+					} else if (cmd == "EDGE:DIR") {
+
+					} else {
+						LogWarning("Unknown command: %s\n", line.c_str());
+					}
+				}
+
+				// Device commands
+				if (subj_dev) {
+					if (cmd == "RATE" && args.size() == 1) {
+						uint64_t rate = stoull(args[0]);
+						set_dev_config<uint64_t>(device, SR_CONF_SAMPLERATE, rate);
+						LogDebug("Updated RATE; now %lu\n", rate);
+					} else if (cmd == "DEPTH" && args.size() == 1) {
+						uint64_t depth = stoull(args[0]);
+						set_dev_config<uint64_t>(device, SR_CONF_LIMIT_SAMPLES, depth);
+						LogDebug("Updated DEPTH; now %lu\n", depth);
+					} else if ((cmd == "START" || cmd == "SINGLE" || cmd == "FORCE") && args.size() == 0) {
+						LogDebug("Starting %s...\n", cmd.c_str());
+						g_triggerOneShot = !(cmd == "START");
+
+						// set_dev_config<bool>(device, SR_CONF_INSTANT, cmd == "FORCE");
+						// This appears in the DSView source, but setting it on the DSO seems to make it
+						// never trigger
+
+						// Actually stopping and starting acquisition for SINGLE is noticeably slow (~0.2s);
+						// Maybe just leave running and send single frames when SINGLE requested?
+
+						g_runAcq = true;
+					} else if (cmd == "STOP" && args.size() == 0) {
+						LogDebug("Stopping...\n");
+						g_runAcq = 0;
+						sr_session_stop();
+					} else {
+						LogWarning("Unknown command: %s\n", line.c_str());
+					}
 				}
 			}
 		}
 	}
 }
 
+
+// float InterpolateTriggerTime(int8_t* buf)
+// {
+// 	if(g_triggerSampleIndex <= 0)
+// 		return 0;
+
+// 	float trigscale = g_roundedRange[g_triggerChannel] / 32512;
+// 	float trigoff = g_offsetDuringArm[g_triggerChannel];
+
+// 	float fa = buf[g_triggerSampleIndex-1] * trigscale + trigoff;
+// 	float fb = buf[g_triggerSampleIndex] * trigscale + trigoff;
+
+// 	//no need to divide by time, sample spacing is normalized to 1 timebase unit
+// 	float slope = (fb - fa);
+// 	float delta = g_triggerVoltage - fa;
+// 	return delta / slope;
+// }
+
+
 void waveform_callback (const struct sr_dev_inst *device, const struct sr_datafeed_packet *packet, void* client_vp) {
 	Socket* client = (Socket*) client_vp;
 
-	if (!g_runAcq) return;
-
 	if (packet->type == SR_DF_HEADER) {
 		struct sr_datafeed_header* header = (struct sr_datafeed_header*)packet->payload;
+		(void) header;
+
+	} else if (packet->type == SR_DF_END) {
+		LogDebug("SR_DF_END; Capture Ended\n");
 
 	} else if (packet->type == SR_DF_TRIGGER) {
 		struct ds_trigger_pos* trigger = (struct ds_trigger_pos*)packet->payload;
@@ -296,14 +366,17 @@ void waveform_callback (const struct sr_dev_inst *device, const struct sr_datafe
 		if (trigger->status & 0x01) {
             uint32_t trig_pos = trigger->real_pos;
 
-            LogNotice("Trigger packet; position=%d\n", trig_pos);
+            LogNotice("Trigger packet; real_pos=%d\n", trig_pos);
 		}
 
 	} else if (packet->type == SR_DF_DSO) {
+		if (!g_runAcq) return;
+		// Don't send further data packets after stop requested
+
 		struct sr_datafeed_dso* dso = (struct sr_datafeed_dso*)packet->payload;
 		size_t num_samples = dso->num_samples;
 
-		// LogDebug("Yield samples: %lu\n", num_samples);
+		LogDebug("Yield samples: %lu\n", num_samples);
 
 		uint8_t* buf = (uint8_t*) dso->data;
 
@@ -364,7 +437,7 @@ void WaveformServerThread()
 
 	for (;;) {
 		while (!g_runAcq) {
-			usleep(10000); //10ms
+			usleep(1000); //1ms
 		}
 
 		LogDebug("Starting Session...\n");
